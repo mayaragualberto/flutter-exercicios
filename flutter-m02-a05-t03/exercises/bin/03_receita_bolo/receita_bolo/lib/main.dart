141434dec12445a0f52906e10fb584e0627875a2
Mayara Gualberto <mayaragualberto@yahoo.com.br>
2022-10-12 15:42:49 -0300

    receita-bolo-alinhamento

--- flutter-m02-a05-t03/exercises/bin/03_receita_bolo/receita_bolo/lib/main.dart
+++ flutter-m02-a05-t03/exercises/bin/03_receita_bolo/receita_bolo/lib/main.dart
@@ -1,6 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Recipe());
 }
 
+class Recipe extends StatelessWidget {
+  const Recipe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.tealAccent, //<-- SEE HERE
+      ),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://i.imgur.com/NrfOeJY.jpg',
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.blue,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
