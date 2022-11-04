8a4c966c9dc3eda017890c0fae023042a899ca04
Mayara Gualberto <mayaragualberto@yahoo.com.br>
2022-11-04 16:55:38 -0300

    readme-e-pastas

--- flutter-m02-a11-t03/exercises/bin/01_lista_infinita/lista_infinita/lib/main.dart
+++ flutter-m02-a11-t03/exercises/bin/01_lista_infinita/lista_infinita/lib/main.dart
@@ -9,6 +9,66 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(100, 8, 254, 188),
+          title: const Text(
+            'Lista "Infinita"',
+            style: TextStyle(
+              color: Color.fromARGB(255, 14, 59, 16),
+              fontSize: 25,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 1000,
+                itemBuilder: (context, index) {
+                  // debugPrint(index.toString());
+                  return ItemContainer(index: index);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemContainer extends StatelessWidget {
+  const ItemContainer({Key? key, required this.index}) : super(key: key);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5.0),
+      padding: const EdgeInsets.all(10.0),
+      alignment: Alignment.center,
+      color: const Color.fromARGB(255, 224, 224, 224),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.star,
+            color: Colors.amber,
+            size: 25,
+          ),
+          Text(
+            'Item nº: $index',
+            style: const TextStyle(
+                fontSize: 24.0, color: Color.fromARGB(255, 26, 51, 27)),
+          ),
+          const SizedBox(),
+        ],
+      ),
+    );
   }
 }
